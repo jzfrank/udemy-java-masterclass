5105a6612b6c70add757765b30b8d69577b105d1
jzfrank <frankjinzhang@gmail.com>
2022-06-29 18:05:39 +0200

    floating number

---
+++ FloatDouble/src/Main.java
@@ -0,0 +1,34 @@
+public class Main {
+    public static void main(String[] args) {
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float maximum value = " + myMaxFloatValue);
+
+        Double myMinDoubleValue = Double.MIN_VALUE;
+        Double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double minimum value = " + myMinDoubleValue);
+        System.out.println("Double maximum value = " + myMaxDoubleValue);
+
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5.00 / 3.00;
+
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("myFloatValue = " + myFloatValue);
+        System.out.println("myDoubleValue = " + myDoubleValue);
+
+        // challenge
+        double weightInPound = 200d;
+        double weightInKilo = weightInPound * 0.45359237d;
+        System.out.println("Converted kilograms = " + weightInKilo);
+
+        double pi = 3.1415927d;
+        double anotherNumber = 3_000_000.4_567_890d;
+
+        System.out.println(pi);
+        System.out.println(anotherNumber);
+
+
+    }
+}
