1dbd82dd0fe436b4ff68b823903b07b853c3d86a
jzfrank <frankjinzhang@gmail.com>
2022-06-30 08:34:38 +0200

    char bool

---
+++ CharBool/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044';
+
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+
+        char myCopyRightChar = '\u00A9';
+        System.out.println(myCopyRightChar);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+        System.out.println(myTrueBooleanValue);
+        System.out.println(myFalseBooleanValue);
+
+        boolean isCustomerOverTwentyOne = true;
+
+
+    }
+}
