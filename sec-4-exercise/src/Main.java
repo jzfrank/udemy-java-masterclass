590eb7f7b5b2195125d7a1317e5d37e64c6c6ab5
jzfrank <frankjinzhang@gmail.com>
2022-07-02 15:43:25 +0200

    finished sec 4

--- sec-4-exercise/src/Main.java
+++ sec-4-exercise/src/Main.java
@@ -5,9 +5,76 @@ public class Main {
 //        System.out.println( isLeapYear(2017) );
 //        System.out.println( isLeapYear(2000) );
 //        System.out.println(areEqualByThreeDecimalPlaces(-3.1756, -3.175));
-        System.out.println(hasTeen(9, 99, 19));
-        System.out.println(hasTeen(23, 15, 42));
-        System.out.println(hasTeen(22, 23, 34));
+
+//        System.out.println(hasTeen(9, 99, 19));
+//        System.out.println(hasTeen(23, 15, 42));
+//        System.out.println(hasTeen(22, 23, 34));
+
+//        System.out.println(area(5.0));
+//        System.out.println(area(-1));
+//        System.out.println(area(5.0,4.0));
+//        System.out.println(area(-1.0,4.0));
+
+//        printYearsAndDays(525600);
+//        printYearsAndDays(1051200);
+//        printYearsAndDays(561600);
+
+//        printEqual(1,1,1);
+//        printEqual(1,1,2);
+//        printEqual(-1,-1,-1);
+//        printEqual(1,2,3);
+
+        System.out.println(isCatPlaying(true,10));
+        System.out.println(isCatPlaying(false,36));
+        System.out.println(isCatPlaying(false,35));
+
+    }
+
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        int lowerLimit = 25;
+        int upperLimit = summer ? 45 : 35;
+        return temperature >= lowerLimit && temperature <= upperLimit;
+    }
+
+    public static void printEqual(int num1, int num2, int num3) {
+        if (num1 < 0 || num2 < 0 || num3 < 0) {
+            System.out.println("Invalid Value");
+        }
+        else if (num1 == num2 && num2 == num3) {
+            System.out.println("All numbers are equal");
+        } else if (num1 != num2 && num1 != num3 && num2 != num3) {
+            System.out.println("All numbers are different");
+        } else {
+            System.out.println("Neither all are equal or different");
+        }
+    }
+
+    public static void printYearsAndDays(long minutes) {
+        if (minutes < 0) {
+            System.out.println("Invalid Value");
+            return;
+        }
+        long YEAR2MINUTES = 525600;
+        long DAY2MINUTES = 1440;
+        long years = minutes / YEAR2MINUTES;
+        long days = (minutes % YEAR2MINUTES) / DAY2MINUTES;
+
+        String message = (minutes + " min = " + years + " y and " + days + " d");
+        System.out.println(message);
+    }
+
+    public static double area(double radius) {
+        if (radius < 0) {
+            return -1;
+        }
+        return Math.PI * radius * radius;
+    }
+
+    public static double area(double x, double y) {
+        if (x < 0 || y < 0) {
+            return -1;
+        }
+        return x * y;
     }
 
     public static boolean shouldWakeUp(boolean barking, int hourOfDay) {
@@ -40,7 +107,7 @@ public class Main {
     }
 
     public static boolean areEqualByThreeDecimalPlaces(double num1, double num2) {
-        return (int)(num1 * 1000) == (int)(num2 * 1000);
+        return (int) (num1 * 1000) == (int) (num2 * 1000);
     }
 
     public static boolean hasEqualSum(int num1, int num2, int num3) {
